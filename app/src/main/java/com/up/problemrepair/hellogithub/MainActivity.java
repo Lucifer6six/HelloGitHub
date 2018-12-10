0c9ff9d57d2edaf6f9ed135cb3020c2bfa0f8c54
lnbs <980366835@qq.com>
2018-12-10 14:26:07 +0800

    mainActivity 修改

--- app/src/main/java/com/up/problemrepair/hellogithub/MainActivity.java
+++ app/src/main/java/com/up/problemrepair/hellogithub/MainActivity.java
@@ -2,12 +2,22 @@ package com.up.problemrepair.hellogithub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.tv);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"hello world",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
